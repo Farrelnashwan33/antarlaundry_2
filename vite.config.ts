d59a6571f5165e4e82cb24fc589ae7bc33505501
Farrelnashwan <akhmadfarrelnashwan831@gmail.com>
2026-08-29 16:49:02 +0700

    upgrade sistem

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import tailwindcss from '@tailwindcss/vite';
-import adapter from '@sveltejs/adapter-cloudflare';
+import cloudflareAdapter from '@sveltejs/adapter-cloudflare';
+import vercelAdapter from '@sveltejs/adapter-vercel';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
@@ -12,7 +13,7 @@ export default defineConfig({
 				runes: ({ filename }) =>
 					filename.split(/[/\\]/).includes('node_modules') ? undefined : true
 			},
-			adapter: adapter()
+			adapter: process.env.VERCEL ? vercelAdapter() : cloudflareAdapter()
 		})
 	]
 });
