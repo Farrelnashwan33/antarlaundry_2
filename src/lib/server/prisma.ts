c8858f1538549054d304244112f11d78f8e38e9a
Farrelnashwan <akhmadfarrelnashwan831@gmail.com>
2026-08-30 18:58:39 +0700

    chore: switch database to local MAMP

--- src/lib/server/prisma.ts
+++ src/lib/server/prisma.ts
@@ -5,10 +5,8 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-// Vercel kadang gagal menyuntikkan env variable, ini fallback darurat
-// Kita gunakan Base64 agar tidak diblokir oleh GitHub Secret Scanning
-const encodedUrl = 'bXlzcWw6Ly9hdm5hZG1pbjpBVk5TX1RsY2tSbW1BODA4RmQ3cTRtc2ZAbXlzcWwtOWExYTE3MC1ha2htYWRmYXJyZWxuYXNod2FuNDItODc2Yi5iLmFpdmVuY2xvdWQuY29tOjI3OTYwL2RlZmF1bHRkYj9zc2wtbW9kZT1SRVFVSVJFRA==';
-const fallbackUrl = Buffer.from(encodedUrl, 'base64').toString('utf-8');
+// Fallback ke database lokal MAMP (hanya bisa jalan di komputer lokal)
+const fallbackUrl = 'mysql://root:root@localhost:8889/antar_laundry';
 const databaseUrl = env.DATABASE_URL || process.env.DATABASE_URL || fallbackUrl;
 
 export const prisma =
